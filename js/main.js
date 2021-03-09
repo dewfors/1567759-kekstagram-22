d56b408bef8cefe707d470d5d6daa944eab34625
Zaur Malakhov <1zaur1@mail.ru>
2021-03-09 11:00:57 +0300

    сделал устранение дребезга

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* global _:readonly */
+
 import {renderPictures, showImgFilters, setFilterDefault, setFilterRandom, setFilterDiscussed} from './draw-photos.js';
 import './upload-image-modal.js';
 import './upload-image-form.js';
@@ -7,13 +9,15 @@ import {getData} from './api.js';
 import {showAlert} from './util.js';
 import {formSubmit} from './upload-image-form.js';
 
+const RERENDER_DELAY = 500;
+
 getData(
   (data) => {
     showImgFilters();
     renderPictures(data);
-    setFilterDefault(() => renderPictures(data));
-    setFilterRandom(() => renderPictures(data));
-    setFilterDiscussed(() => renderPictures(data));
+    setFilterDefault(_.debounce(() => renderPictures(data),RERENDER_DELAY));
+    setFilterRandom(_.debounce(() => renderPictures(data),RERENDER_DELAY));
+    setFilterDiscussed(_.debounce(() => renderPictures(data),RERENDER_DELAY));
   },
   () => showAlert('Не удалось получить данные с сервера. Попробуйте позже.'));
 
